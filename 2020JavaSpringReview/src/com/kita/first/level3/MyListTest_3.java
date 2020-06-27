d6b2cd9a9932528304b01e8c79a1088abd3dfc20
phantom-black <black_phantom@naver.com>
2020-06-27 22:19:36 +0900

    20200627 자바 복습

--- 2020JavaSpringReview/src/com/kita/first/level3/MyListTest_3.java
+++ 2020JavaSpringReview/src/com/kita/first/level3/MyListTest_3.java
@@ -2,14 +2,13 @@ package com.kita.first.level3;
 
 import java.util.Arrays;
 
-public class MyListTest_2 {
-	/*
+public class MyListTest_3 {
 	public static void main(String[] args) {
 		MyList list = new MyList();
 		list.add(10);
 		list.add(15);
 		list.add(20);
-		list.add(1, 1200);	// 10, 15, 20 ? 10 , 1200, 15, 20
+		list.add(1, 1200);
 		
 		int len = list.size(); //2
 		int val = list.get(1); //15
@@ -19,15 +18,18 @@ public class MyListTest_2 {
 		//int delVal = list.remove(); // 마지막 값 지우고 지우는 값 리턴
 		int delVal = list.remove(2);
 		
-		MyArrays.print(list); // [10, 1200, 15] 프린트 되도록!
-		String str = MyArrays.toString(list); // [10, 1200, 15] 문자열 리턴하도록!
-		// Arrays.toString 메소드 사용금지! 구현하기
-		System.out.println(str);
+		//MyArrays.print(list); // [10, 1200, 15] 프린트 되도록
+		String str = MyArrays.toString(list); // [10, 1200, 15] 문자열 리턴
+		
+		System.out.println("str : " + str);
+		
+		//Arrays.toString 메소드 사용금지! 구현하기
 	}
 }
 
 class MyArrays {
-	static void print(MyList arr) {	// MyArrays 호출하지 않아도 사용할 수 있도록 메모리에 올라갈 수 있게 static 붙임
+	
+	public static void print(MyList arr) { // ***호출하지 않아도 메모리에 올라가 있어야 바로 사용 가능하므로 static 붙이기!!
 		int[] temp = new int[arr.size()];
 		for(int i=0; i<arr.size(); i++) {
 			temp[i] = arr.get(i);
@@ -35,44 +37,45 @@ class MyArrays {
 		System.out.println(Arrays.toString(temp));
 	}
 	
-	static String toString(MyList arr) {
-		String str = "";
+	public static String toString (MyList arr) {
+		int[] temp = new int[arr.size()];
+		String str="";
 		for(int i=0; i<arr.size(); i++) {
-			if(i>0) {str+=", ";}
-			str+=arr.get(i);
+			temp[i] = arr.get(i);
 		}
-		//return "["+str+"]";
+		for(int i=0; i<temp.length; i++) {
+			if (i>0) {
+				str += ", ";
+			}
+			str += temp[i];
+		}
+		//return "[" + str + "]";
 		return String.format("[%s]", str);
-				
 	}
 }
 
 class MyList {
-	private int[] arr; 	// 프라이빗 전역변수 선언
+	private int[] arr;
 	
 	MyList() {
 		init();
 	}
 	
-	private void init() { 	// arr 안에 들어가는 값도 private이어야!
+	private void init() {
 		arr = new int[0];
 	}
 	
 	public void add(int n) {
-		int[] temp = new int[arr.length+1];
-		for(int i=0; i<arr.length; i++) {
-			temp[i]=arr[i];
-		}
-		temp[arr.length] = n;
-		arr=temp;
+		add(arr.length, n);
 	}
 	
 	public void add(int idx, int n) {
 		int[] temp = new int[arr.length+1];
-		for(int i=0; i<temp.length; i++) {
-			temp[i] = (i<idx) ? (arr[i]) : (arr[i-1]);
+		for(int i=0; i<arr.length; i++) {
+			temp[(i<idx) ? (i) : (i+1)] = arr[i];
 		}
-		temp[idx]=n;
+		temp[idx] = n;
+		arr = temp;
 	}
 	
 	public int size() {
@@ -83,14 +86,18 @@ class MyList {
 		return arr[idx];
 	}
 	
+	public int remove() {
+		return remove(arr.length-1); // 리턴 안에 넣어주기!
+	}
+	
 	public int remove(int idx) {
 		int[] temp = new int[arr.length-1];
+		int n;
 		for(int i=0; i<temp.length; i++) {
-			temp[(i<idx) ? (i):(i+1)]=arr[i];
+			temp[i] = arr[(i<idx) ? (i) : (i+1)];
 		}
-		int n = arr[idx];
+		n = arr[idx];
 		arr=temp;
 		return n;
 	}
-	*/
 }
